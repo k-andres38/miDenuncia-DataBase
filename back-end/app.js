7d3ae1c4e88e13da70800b10489b3b0caa24f36e
Carlos Navarro <kandres38@gmail.com>
2023-03-25 17:23:59 -0500

    creacion googlAuth20 y su ruta

--- back-end/app.js
+++ back-end/app.js
@@ -3,13 +3,7 @@ const express = require('express')
 const app = express()
 
 
-// const port= 4000
 
-// app.listen(port, () => {
-//   console.log('El servidor está escuchando en el puerto ' + port);
-// });
-////aqui hacemos las importaciones y que todo quede dentro de ella
-//const express = require('express');
 
 
 module.exports = app;
