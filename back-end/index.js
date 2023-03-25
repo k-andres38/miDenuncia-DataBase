3179b2ee327999abeeb5d166d2936585983e285c
Carlos Navarro <kandres38@gmail.com>
2023-03-25 17:00:45 -0500

    creacion googlAuth20 y su ruta

--- back-end/index.js
+++ back-end/index.js
@@ -1,84 +1,84 @@
 
-const app=require('./app')
+//const app=require('./app')
 
 
-////aqui hacemos las importaciones y que todo quede dentro de ella
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors')
-const sgMail=require('./services/sendgrid')
-const session = require('express-session');
-//const { auth } = require('express-openid-connect');
-const morgan = require('morgan')
-dotenv.config()
+// ////aqui hacemos las importaciones y que todo quede dentro de ella
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const cors = require('cors')
+// const sgMail=require('./services/sendgrid')
+// const session = require('express-session');
+// //const { auth } = require('express-openid-connect');
+// const morgan = require('morgan')
+// dotenv.config()
 
-const passport= require('passport');
+// const passport= require('passport');
 
-//FIN
-////////////////////////////////////////////////////////////////
+// //FIN
+// ////////////////////////////////////////////////////////////////
 
-///aqui se LLAMAN A A LAS RUTAS las rutas generales y que todo quede dentro de ella
-const routes = require('./routes/routeUsers/route')
-const routesComment = require('./routes/routeComments/route')
-const routeRequest=require('./routes/routeRequest/route')
+// ///aqui se LLAMAN A A LAS RUTAS las rutas generales y que todo quede dentro de ella
+// const routes = require('./routes/routeUsers/route')
+// const routesComment = require('./routes/routeComments/route')
+// const routeRequest=require('./routes/routeRequest/route')
 
-//FIN
-////////////////////////////////////////////////////////////////
-
-
-/// codigo especial para procesar solicitudes HTTP y expres json lo convierta en json
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-
-app.use(cors()); //proteccion de cabecera
-//app.use(morgan('tiny'));//monitoreo de solicitudes
-////////////////////////////////////////////////////////////////
-
-///aqui se configura la ENTRADA  A LAS RUTAS trabajando  solo rutas
-
-app.use('/',routes)
-app.use('/',routeRequest)
-app.use('/', routesComment)
-
-//FIN
-////////////////////////////////////////////////////////////////
+// //FIN
+// ////////////////////////////////////////////////////////////////
 
 
+// /// codigo especial para procesar solicitudes HTTP y expres json lo convierta en json
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+
+// app.use(cors()); //proteccion de cabecera
+// //app.use(morgan('tiny'));//monitoreo de solicitudes
+// ////////////////////////////////////////////////////////////////
+
+// ///aqui se configura la ENTRADA  A LAS RUTAS trabajando  solo rutas
+
+// app.use('/',routes)
+// app.use('/',routeRequest)
+// app.use('/', routesComment)
+
+// //FIN
+// ////////////////////////////////////////////////////////////////
 
 
-////////////////////////////////////////////////////////////////
-//INICIO
-//codigo especial para los usuarios de GOOGLE
-app.use(session({
-  secret: 'MIDENUNCIA',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 
-require('./middleware/auth2UserGoogle')
+// ////////////////////////////////////////////////////////////////
+// //INICIO
+// //codigo especial para los usuarios de GOOGLE
+// app.use(session({
+//   secret: 'MIDENUNCIA',
+//   resave: false,
+//   saveUninitialized: false
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 
-app.get('/google',
-  passport.authenticate('google', { scope: ['profile','email'] }));
+// require('./middleware/auth2UserGoogle')
 
-app.get('/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),(req, res)=> {
+
+// app.get('/google',
+//   passport.authenticate('google', { scope: ['profile','email'] }));
+
+// app.get('/google/callback', 
+//   passport.authenticate('google', { failureRedirect: '/login' }),(req, res)=> {
  
-    res.status(200).json({message: 'success',data:req.user});
+//     res.status(200).json({message: 'success',data:req.user});
     
-  });
+//   });
 
   
-//FIN
-////////////////////////////////////////////////////////////////
+// //FIN
+// ////////////////////////////////////////////////////////////////
 
  
 
-const port= 4000
+// const port= 4000
 
-app.listen(port, () => {
-  console.log('El servidor está escuchando en el puerto ' + port);
-});
+// app.listen(port, () => {
+//   console.log('El servidor está escuchando en el puerto ' + port);
+// });
