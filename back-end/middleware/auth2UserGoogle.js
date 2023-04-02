bd57f6530d896a431e6c7e897b1e7dd88f697fee
Carlos Navarro <kandres38@gmail.com>
2023-04-01 20:23:43 -0500

    pruebas de ajustes

--- back-end/middleware/auth2UserGoogle.js
+++ back-end/middleware/auth2UserGoogle.js
@@ -18,7 +18,7 @@ passport.deserializeUser((user,done)=> {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/google/callback"
+    callbackURL: "https://midenuncia-database-production.up.railway.app/google/callback"
 
    
   },
