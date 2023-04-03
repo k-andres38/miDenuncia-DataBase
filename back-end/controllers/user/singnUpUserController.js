c997ee2b6ce0b9faca00044d962322b69ffcd744
Carlos Navarro <kandres38@gmail.com>
2023-04-02 22:24:32 -0500

    pruebas de ajustes

--- back-end/controllers/user/singnUpUserController.js
+++ back-end/controllers/user/singnUpUserController.js
@@ -25,8 +25,8 @@ exports.signUp = async(req,res,next)=>{
                    let token= jwt.sign({
                         data
                       }, process.env.JWT_SECRET, { expiresIn: '1h' });
-                      data.token=token;
-                      data.save()
+                    //   data.token=token;
+                    //   data.save()
                     
                     res.status(201).json({data,token})
                 }).catch((err) => {
