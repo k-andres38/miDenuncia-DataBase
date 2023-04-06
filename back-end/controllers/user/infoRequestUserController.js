ba84f4366bf63b5b9d85aec981a4e7eab33faf51
Carlos Navarro <kandres38@gmail.com>
2023-04-05 20:23:58 -0500

    ajustes de base de datos

--- back-end/controllers/user/infoRequestUserController.js
+++ back-end/controllers/user/infoRequestUserController.js
@@ -6,11 +6,11 @@ const modelRating = require('../../models').rating
 exports.infoRequestUser = async (req,res,next) => {
 
     try {
-    let storeRequest= await modelRequest.findAll({include: [modelTypeRequest,modelUser]})
+    let news= await modelRequest.findAll({include: [modelTypeRequest,modelUser]})
 
     // let storeUser= await modelRating.findAll({include: [modelUser]});
-
-    res.status(200).send(JSON.stringify({storeRequest,storeUser}))
+        res.status(200).json(news)
+    //res.status(200).send(JSON.stringify({storeRequest,storeUser}))
     } catch (error) {
         res.send(error)
     }
