9a34e5e384c1010f88066bd3377b111be825a9e2
Carlos Navarro <kandres38@gmail.com>
2023-04-05 20:40:21 -0500

    ajustes de base de datos

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -48,7 +48,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'user',
-    paranoid: true,
+    // paranoid: true,
     // validate: {
     //   userValidation() {
     //     if (this.name.length < 2) {
