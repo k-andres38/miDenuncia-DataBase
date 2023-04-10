12fbd28e422d36cfc283e0834c51143039822602
Carlos Navarro <kandres38@gmail.com>
2023-04-10 17:42:23 -0500

    ajustes de request

--- back-end/controllers/createRequest/requestController.js
+++ back-end/controllers/createRequest/requestController.js
@@ -1,4 +1,5 @@
 const { request } = require('express');
+const { find, findOne } = require('../../modeloMongo/sessionMongodb');
 const modelsTypeRequest = require('../../models').types_request;
 const modelsDocument = require('../../models').document;
 const modelsUser = require('../../models').user;
@@ -8,23 +9,47 @@ const modelsPhoto = require('../../models').photo;
 exports.request = async (req, res, next) => {
 
     try {
-        let { name, type, number_document, place_dispatch, address, contact_phone, location, neighborhood, subject, problem, solution, url, document_id} = req.body;
+        let { name,
+            type,
+            number_document,
+            place_dispatch,
+            address,
+            contact_phone,
+            location,
+            neighborhood, 
+            subject, 
+            problem, 
+            solution, 
+            url, 
+            type_request_id } = req.body;
 
 
         await modelsUser.findByPk(req.params.id).then(user => {
-            
+
             modelsTypeRequest.create({ name })
 
-            modelsDocument.create({id:number_document, type, place_dispatch })
+            modelsDocument.create({ id: number_document, type, place_dispatch })
 
-            user.update({where:{ address, contact_phone, document_id:number_document }, address, contact_phone, document_id:number_document })
+            user.update({ where: { address, contact_phone, document_id: number_document }, address, contact_phone, document_id: number_document })
 
-            modelsRequest.create({ location, neighborhood, subject, problem, solution })
+            modelsRequest.create({ location, neighborhood, subject, problem, solution, type_request_id, user_id:req.params.id})
 
-            modelsPhoto.create({ url })
-            res.status(200).json({user})
-        }).catch(error=>{
-            res.status(400).json({message:'no se pudo completrar la soliciud'})
+            const requesPhotos = modelsRequest.findOne({where:{user_id:req.params.id}})
+            if(requesPhotos){
+                modelsPhoto.create({
+                    url,
+                    request_id:requesPhotos.id,
+                    
+
+                })
+            }
+
+
+
+            modelsPhoto.create({ url,  })
+            res.status(200).json({ user })
+        }).catch(error => {
+            res.status(400).json({ message: 'no se pudo completrar la soliciud' })
         })
 
 
