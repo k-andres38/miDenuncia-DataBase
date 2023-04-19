395e7367055e15ada53955c67167d14a66a38c0c
Dayanna Rojas <rojasdahianna123@gmail.com>
2023-04-19 00:27:06 -0500

    correo

--- back-end/controllers/createRequest/requestController.js
+++ back-end/controllers/createRequest/requestController.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 dotenv.config()
 const pdfkit = require('pdfkit');
 const fs = require('fs');//con fs podemos leer y escribir
-
+const axios = require('axios');
 const doc = new pdfkit();
 //AQUI TERMINAN LOS SERVICIOS
 
@@ -96,8 +96,174 @@ exports.request = async (req, res, next) => {
     doc.moveDown(0.5);
     
     /*doc.text(`Nombre: ${req.body.name}`);*/
+
+    doc.fontSize(12).text('Señores:', {color: "#000000"});
+
+
+doc.fontSize(16).text('Alcaldía de Bucaramanga', { 
+  align: 'left', 
+  width: 410, 
+  lineGap: 2, 
+ 
+  lineHeight: 1.2 
+});
+
+doc.fontSize(12).text('E. S. D.', { 
+        align: 'left',
+        lineHeight: 1.2,
+        color: 'black'
+   });
+
+
+   doc.fontSize(12).text("\n");
+
+
+  doc
+  .fontSize(12)
+  .text('Ref.: (tipo de petición)  ', {
+       align: 'left',
+       lineGap: 1.2,
+       characterSpacing: 1
+  })
+
+  doc.fontSize(12).text("\n")
+
+  doc
+  .fontSize(16)
+  .text('ANDRES FELIPE LEON PLATA, ', {continued: true})
+  .fontSize(12)
+  .text('mayor de edad, vecino de esta ciudad, identificado con cédula de ciudadanía número 1.098.741.421 expedida en Bucaramanga, me permito instaurar en nombre propio ante su despacho este Derecho de Petición, con fundamento en los siguientes:', {
+  
+    continued: false
+});
+
+  
+
+  doc.fontSize(12).text("\n")
+
+  doc.fontSize(16).text("PROBLEMA", { align: "center"})
    
-    doc.text(`Tipo: ${req.body.type} `);
+ doc .fontSize(12).text("\n")
+
+
+  doc.fontSize(16).text('PRIMERO: ', { continued: true})
+  .fontSize(12).text('Para el mes de Julio del año en curso me acerque a las instalaciones de BANCOLOMBIA sucursal las palmas de Bucaramanga para adquirir una cuenta bancaria...');
+  
+  doc .fontSize(12).text("\n")
+
+  doc.fontSize(16).text("PETICIÓN (solución)", { align: "center"})
+
+  doc .fontSize(12).text("\n")
+
+
+  doc.fontSize(16).text('PRIMERO: ', { continued: true})
+  .fontSize(12).text('Hacer efectiva la solicitud de cancelación de seguro ');
+
+
+doc .fontSize(12).text("\n")
+
+doc.fontSize(16).text("FUNDAMENTOS DE DERECHO", { align: "center"})
+
+doc .fontSize(12).text("\n")
+
+
+doc.list([
+  'La Constitución colombiana, artículo 23, sobre Derecho de petición',
+  'Esperar más información…'
+], {
+  align: 'justify',
+  fontSize: 12,
+  listItemGap: 6,
+  TextIndent: 30,
+  /*bulletRadius: 8*/
+  listType: 'numbered',
+});
+
+doc .fontSize(12).text("\n")
+
+doc.fontSize(16).text("ANEXOS", { align: "center"})
+
+doc .fontSize(12).text("\n")
+
+doc.text('La petición que elevo mediante este escrito se fundamenta en las razones de hecho y de derecho anteriormente expuestas, que se dejan debidamente comprobadas con los siguientes anexos y documentos:', { 
+  align: 'justify', 
+  fontSize: 12, 
+}).text('\n');
+
+
+doc.fontSize(16).text("PRUEBAS", { align: "center"})
+doc .fontSize(12).text("\n")
+
+
+doc.text('Para la resolución favorable de mi petición, solicito tener como pruebas los anexos que relacioné antes y practicar, en caso de que lo encuentre viable cualquier otro medio de prueba que, a su juicio, sea necesario para la resolución favorable de mi petición.', { 
+  align: 'justify', 
+  fontSize: 12, 
+}).text('\n');
+
+doc.fontSize(16)
+   .text('Fotos de la petición', {
+     align: 'justify',
+     lineGap: 12
+   });
+
+  //necesito una url funcional para probar la url que llega del body y cambiar a 68
+   /*
+
+axios.get(cloudPhoto, { responseType: 'arraybuffer' })
+.then(response => {
+
+  const imageBuffer = Buffer.from(response.data, 'binary');
+
+  // Carga la imagen en el PDF
+  doc.image(imageBuffer,  {
+
+    fit: [250, 300], 
+    align: 'center',
+    valign: 'center' 
+
+  });
+
+ 
+})
+.catch(error => {
+  console.error(error);
+});
+
+
+/*
+const urrl = fs.readFileSync('/ruta/de/la/imagen.jpg');
+
+   doc.image(url, {
+    fit: [250, 300], 
+    align: 'center',
+    valign: 'center' 
+  });*/
+
+/*
+
+  const imageUrl = 'https://res.cloudinary.com/your-cloud-name/image/upload/v1626680727/your-image.jpg';
+const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+const imageBuffer = Buffer.from(response.data, 'binary');
+doc.image(imageBuffer, { width: 300 });*/
+
+  doc .fontSize(12).text("\n")
+  doc.fontSize(16).text("NOTIFICACIONES", { align: "center"})
+  doc .fontSize(12).text("\n")
+
+  doc.fontSize(12).text('Recibiré notificaciones personales preferiblemente en la Calle 43 # 7-57 Edificio Palermo 2 Apartamento 508, Barrio Alfonso López, Bucaramanga y al correo electrónico ', { align: 'justify', continued:true })
+ .fontSize(16).text('ing.andresleon@hotmail.com', { link: 'ing.andresleon@hotmail.com',  color: 'blue', underline: false })
+ 
+ doc .fontSize(12).text("\n")
+ doc.fontSize(12).text("Atentamente,", { align: "justify"})
+
+ doc .fontSize(12).text("\n")
+
+ doc.fontSize(16).text('ANDRES FELIPE LEON PLATA ', { align: 'left'})
+ doc.fontSize(12).text("1.454.999.222 Bucaramanga", { align: "justify"})
+
+  doc.fontSize(12).text("\n")
+
+doc.text(`Tipo: ${req.body.type} `);
     doc.text(`Número de documento: ${req.body.number_document}`);
     doc.text(`Lugar de expedición: ${req.body.place_dispatch}`);
     doc.text(`Dirección: ${req.body.address}`);
@@ -170,6 +336,8 @@ exports.request = async (req, res, next) => {
                   folder: 'peticiones'
                 })
                 // console.log(__dirname+'Cap');
+               
+               
 
                 cloudPhoto.then((data) => {
                   console.log(data);
@@ -181,7 +349,10 @@ exports.request = async (req, res, next) => {
                     Crop: 'fill'
                   });
 
-                
+
+
+                 
+
                   modelsPhoto
                     .create({
                       url: data.secure_url,
@@ -214,7 +385,7 @@ exports.request = async (req, res, next) => {
                           
 
                           const mailOptions = {
-                            to: "kandres38@gmail.com",
+                            to: "midenunciacoex@gmail.com",
                             subject: `Denuncia por`,
                             text:`\n\n aqui el pie: gracias por usar nuestros servicios <3`,
                             html: `<h1 style="color: #5e9ca0;"><span style="color: #800080;">bienvenido a mi denuncia!</span></h1>
