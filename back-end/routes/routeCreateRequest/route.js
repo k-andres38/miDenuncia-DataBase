528bc15809207f92a0eebbb48460092860d65a03
Carlos Navarro <kandres38@gmail.com>
2023-04-20 16:28:32 -0500

    ajustes

--- back-end/routes/routeCreateRequest/route.js
+++ back-end/routes/routeCreateRequest/route.js
@@ -1,12 +1,12 @@
 const express=require('express');
 const router= express.Router();
 
-const multer=require('multer')
+// const multer=require('multer')
 
-const upload = multer();
+// const upload = multer();
 
 const requestController = require('../../controllers/createRequest/requestController');
 
-router.put('/request/:id',upload.array('image[]'), requestController.request)
+router.put('/request/:id', requestController.request)
 
 module.exports = router; // --> creado por farit
