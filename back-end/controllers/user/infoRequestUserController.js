3c28391e93fd74b4ec5ca186b6e960ab9bd670d8
Carlos Navarro <kandres38@gmail.com>
2023-04-24 00:03:52 -0500

    prueba

--- back-end/controllers/user/infoRequestUserController.js
+++ back-end/controllers/user/infoRequestUserController.js
@@ -17,76 +17,8 @@ exports.infoRequestUser = async (req, res, next) => {
 
     const currentPage = parseInt( req.query.offset) || 1;
     const limit = parseInt( req.query.limit) || 10000;
-    const offset = (currentPage  ) * 5;
-  //   //console.log(currentPage,limit)
+    const offset = currentPage * 5;
 
-  //  // if(req.query.filtro){
-  //   //  console.log(req.query.filtro)
-  //     await modelsRequest
-  //     .findAll({
-  //       // limit: 5,
-  //       // offset: 0,
-  //       limit,
-  //       offset,
-
-      
-        
-  //       order: [["id", "DESC"]],
-  //       attributes: [
-  //         "id",
-  //         "location",
-  //         "neighborhood",
-  //         "subject",
-  //         "problem",
-  //         "solution",
-         
-  //         "status",
-  //         "tag",
-  //         "createdAt",
-         
-  //         [
-  //           Sequelize.literal(
-  //             `(SELECT COUNT(*) FROM comments WHERE comments.request_id = request.id AND comments.deletedAt IS NULL)`
-  //           ),
-  //           "comment_count",
-  //         ],[
-  //           Sequelize.literal(
-  //             `(SELECT COUNT(*) FROM supports WHERE supports.request_id = request.id)`
-  //           ),
-  //           "support",
-  //         ],
-  //       ],
-
-  //       include: [
-  //         {
-  //           model: modelsTypeRequest,
-  //           attributes: ["id", "name"],
-  //         },
-  //         {
-  //           model: modelUser,
-  //           attributes: ["id", "nickname", "name"],
-  //         },
-  //         {
-  //           model: modelsComment,
-  //           attributes: ["id", "description"],
-  //           include: [
-  //             { model: modelUser, attributes: ["id", "nickname", "name"] },
-  //           ],
-  //         },
-  //         {
-  //           model: modelsPhoto,
-  //           attributes: ["id", "url"],
-  //         },
-  //       ],
-  //       where:{type_request_id:req.query.filtro}
-  //     }).then((news) => {
-  //       //res.redirect('http://localhost:5173/').json({ news })
-  //       res.status(200).json({ news });
-  //       // console.log(publicaciones);
-  //     });
-
-    
-   // }else{
       await modelsRequest
       .findAll({
         // limit: 5,
